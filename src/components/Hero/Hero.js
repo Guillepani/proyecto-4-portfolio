12b429fd1d754c6e8e52a021a1f336538eba9bc7
Guille <guillempaniagua@gmail.com>
2026-03-16 09:06:30 +0000

    feat: crear componente Hero y conectarlo al proyecto

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,10 @@
+import './Hero.css'
+
+export const Hero = () => {
+  return `
+    <section class="hero">
+      <h2>Hola, soy Guillem</h2>
+      <p>Desarrollador web</p>
+    </section>
+  `
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './style.css'
 import { Header } from './components/Header/Header.js'
+import { Hero } from './components/Hero/Hero.js'
 
 const app = document.querySelector('#app')
 
 app.innerHTML = `
   ${Header()}
+  ${Hero()}
 `
