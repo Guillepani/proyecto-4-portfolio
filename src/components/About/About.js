49ad534a0a7aea87778e9a0e6cc9991aaf6f98fe
Guille <guillempaniagua@gmail.com>
2026-03-16 09:12:25 +0000

    feat: añadir sección About al portfolio

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import './About.css'
+
+export const About = () => {
+  return `
+    <section class="about">
+      <h2>Sobre mí</h2>
+      <p>
+        Soy Guillem, desarrollador web en formación con interés en crear
+        aplicaciones útiles y bien diseñadas.
+      </p>
+    </section>
+  `
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './style.css'
 import { Header } from './components/Header/Header.js'
 import { Hero } from './components/Hero/Hero.js'
+import { About } from './components/About/About.js'
 
 const app = document.querySelector('#app')
 
 app.innerHTML = `
   ${Header()}
   ${Hero()}
+  ${About()}
 `
